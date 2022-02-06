637768bb22551623495976282ba28a31936ff862
Harshit Jaiswal <harshitjaiswal@Harshits-MacBook-Air.local>
2022-02-06 13:03:47 +0530

    Alternative : WebClient Way of making calls

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
@@ -11,6 +12,11 @@ public class MovieCatalogServiceApplication {
 	public RestTemplate getRestTemplate() {
 		//Bean -> create single instance (singleton) of any object and have it shared across multiple other classes.
 		return new RestTemplate(); //  https://stackoverflow.com/questions/4721279/please-explain-resttemplate
+	}
+
+	@Bean
+	public WebClient.Builder getWebClientBuilder() {
+		return WebClient.builder();
 	}
 
 	public static void main(String[] args) {
--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/CatalogResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -21,6 +22,9 @@ public class CatalogResource {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
         //1. Get all rated movie id
@@ -33,6 +37,17 @@ public class CatalogResource {
         );
         return ratings.stream()
                 .map(rating -> {
+                    /*
+                    Alternative WebClient Way. Reactive side.
+
+                    Movie movie = webClientBuilder.build()
+                            .get()
+                            .uri("http://localhost:8082/movies/"+rating.getMovieId())
+                            .retrieve()
+                            .bodyToMono(Movie.class)
+                            .block();
+                     */
+
                     //return response is string, unmarshalling of response to Movie object.
                     Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
                     return new CatalogItem(movie.getName(), "Description", rating.getRating());
